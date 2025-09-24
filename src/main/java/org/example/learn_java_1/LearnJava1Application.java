15bb7049f24013fd754dfe1645de6ed24074e1db
Nga <ngatt33@ghtk.co>
2025-09-24 10:32:52 +0700

    first commit

---
+++ src/main/java/org/example/learn_java_1/LearnJava1Application.java
@@ -0,0 +1,13 @@
+package org.example.learn_java_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJava1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJava1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/learn_java_1/LearnJava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.learn_java_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJava1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
